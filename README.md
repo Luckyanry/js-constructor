f1ca255c892026a4e259f5e9f24ee0df0afb1f38
Andrii Kochmaruk <luckyanry@gmail.com>
2020-10-03 18:59:40 +0200

    uploaded project to firebase

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js-constructor
+
+# For adding changes to project:
+
+# 1. npm run build
+
+# 2. firebase deploy
--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -610,7 +610,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50351" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62834" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
